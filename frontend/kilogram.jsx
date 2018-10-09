9ef817af8dc6f8c28e77f3c43e538ec9a732cea7
tonyjhuang <huangtonyj91@gmail.com>
2018-10-09 16:15:19 -0700

    Initialize React and frontend Hello World

---
+++ frontend/kilogram.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById("root");
+
+  ReactDOM.render(
+    <h1>Hellooooooo</h1>,
+    root
+  );
+
+});
