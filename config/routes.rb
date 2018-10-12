9a82922acfbbf5865a9f6e6f75a61077e49f8e5e
tonyjhuang <huangtonyj91@gmail.com>
2018-10-12 16:28:01 -0700

    Clean up backend for FOLLOW

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,17 @@ Rails.application.routes.draw do
   root "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
+
     resources :users, only: [:create, :show]
-    
+
     resource :session, only: [:create, :destroy]
 
     resources :posts, only: [:create, :show, :index, :update, :destroy]
 
     resources :likes, only: [:create, :destroy]
 
-    resources :follows, only: [:create, :destroy]
+    resources :follows, only: [:create]
+
   end
 
 end
