52d7571f4ae4f03b25a4777f1b8b42e4fb723c64
tonyjhuang <huangtonyj91@gmail.com>
2018-10-16 09:25:16 -0700

    Test new relative path for heroku

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-import KilogramLogo from '../../../app/assets/images/kilogram_logo.png';
-// import KilogramLogo from '/app/assets/images/kilogram_logo.png';
-import AppleiOSAppStoreLogo from '../../../app/assets/images/ios_app_store.png';
-import GooglePlayStoreLogo from '../../../app/assets/images/google_play_store.png';
+import KilogramLogo from '/app/assets/images/kilogram_logo.png';
+import AppleiOSAppStoreLogo from '/app/assets/images/ios_app_store.png';
+import GooglePlayStoreLogo from '/app/assets/images/google_play_store.png';
+
+// import KilogramLogo from '../../../app/assets/images/kilogram_logo.png';
+// import AppleiOSAppStoreLogo from '../../../app/assets/images/ios_app_store.png';
+// import GooglePlayStoreLogo from '../../../app/assets/images/google_play_store.png';
 
 
 class SessionForm extends React.Component {
